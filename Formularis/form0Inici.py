efc0a01fee1f07ab5eb3efae3a1390a489fbcb86
Bayo <jbayo@iesebre.com>
2023-02-09 19:25:40 +0100

    Classe 09/02/2023

--- Formularis/form0Inici.py
+++ Formularis/form0Inici.py
@@ -6,25 +6,41 @@ from Formularis.form1Protagonist import updateProtagonistScreen
 connexio = None
 
 
-def openToProtagonistMenu():
+def openLocationsFromMenu():
+    frameMenu.pack_forget()
+    frameLocations.pack()
 
+
+def backToMenufromLocations():
+    frameLocations.pack_forget()
+    frameMenu.pack()
+
+def openRoadsFromMenu():
+    frameMenu.pack_forget()
+    frameRoads.pack()
+
+
+def backToMenufromRoads():
+    frameRoads.pack_forget()
+    frameMenu.pack()
+
+def openObjectsFromMenu():
+    frameMenu.pack_forget()
+    frameObjects.pack()
+
+
+def backToMenufromObjects():
+    frameObjects.pack_forget()
+    frameMenu.pack()
+
+def openProtagonistFromMenu():
     frameMenu.pack_forget()
     frameProtagonist.pack()
-    pass
 
 
-def openToObjectsMenu():
-    pass
-
-
-def openToRoadsMenu():
-    pass
-
-
-def openToLocationsMenu():
-    pass
-
-
+def backToMenufromProtagonist():
+    frameProtagonist.pack_forget()
+    frameMenu.pack()
 def updateProtagonist():
     updateProtagonistScreen()
 
@@ -38,20 +54,19 @@ frameMenu = tk.Frame(window)
 frameMenu.pack()
 buttonLocations = tk.Button(frameMenu,
                             text="LOCALITZACIONS",
-                            command=openToLocationsMenu)
+                            command=openLocationsFromMenu)
 buttonRoads = tk.Button(frameMenu,
                         text="CAMINS",
-                        command=openToRoadsMenu)
+                        command=openRoadsFromMenu)
 buttonObjects = tk.Button(frameMenu,
                           text="OBJECTES",
-                          command=openToLocationsMenu)
+                          command=openObjectsFromMenu)
 buttonProtagonist = tk.Button(frameMenu,
                               text="PROTAGONISTA",
-                              command=openToProtagonistMenu)
+                              command=openProtagonistFromMenu)
 buttonGetOut = tk.Button(frameMenu,
-                              text="SORTIR",
-                              command=leaveGame)
-
+                         text="SORTIR",
+                         command=leaveGame)
 
 buttonLocations.pack()
 buttonRoads.pack()
@@ -61,11 +76,33 @@ buttonGetOut.pack()
 
 frameProtagonist = tk.Frame(window)
 
-buttonGetOut = tk.Button(frameProtagonist,
-                              text="SORTIR",
-                              command=leaveGame)
 
-buttonGetOut.pack()
+buttonGetOutFromProtagonist = tk.Button(frameProtagonist,
+                                        text="SORTIR",
+                                        command=backToMenufromProtagonist)
+buttonGetOutFromProtagonist.pack()
 
+
+frameLocations = tk.Frame(window)
+buttonGetOutFromLocations = tk.Button(frameLocations,
+                                      text="SORTIR",
+                                      command=backToMenufromLocations)
+buttonGetOutFromLocations.pack()
+
+
+frameRoads = tk.Frame(window)
+buttonGetOutFromRoads = tk.Button(frameRoads,
+                                  text="SORTIR",
+                                  command=backToMenufromRoads)
+
+buttonGetOutFromRoads.pack()
+
+
+frameObjects = tk.Frame(window)
+buttonGetOutFromObjects = tk.Button(frameObjects,
+                                    text="SORTIR",
+                                    command=backToMenufromObjects)
+
+buttonGetOutFromObjects.pack()
 
 window.mainloop()
